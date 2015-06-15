d6ded8cd6523fd5cdef6d452700770e4c90dd0da
cwelc1 <cwelch2@sapient.com>
2015-06-15 07:10:21 -0700

    first commit

---
+++ scripts/jquery.dropdown.js
@@ -0,0 +1,68 @@
+var maxHeight = 280;
+
+$(function(){
+
+    /* Populate the dropdown list with years */
+    var curYear = new Date().getFullYear(); 
+    if (curYear === '' || curYear === undefined || curYear === null){
+        curYear = 2015; 
+    }
+    for (i = 0; i <= 80; i++){
+        $(".sub_menu").append("<li><a href='#'>" + curYear + "</a></li>"); 
+        curYear--; 
+    }
+
+    $(".ddl > li").hover(function() {
+         var $container = $(this),
+             $list = $container.find("ul"),
+             $anchor = $container.find("a"),
+             height = $list.height() * 1.1,       // make sure there is enough room at the bottom
+             multiplier = height / maxHeight;     // needs to move faster if list is taller
+
+        // need to save height here so it can revert on mouseout            
+        $container.data("origHeight", $container.height());
+        
+        // so it can retain it's rollover color all the while the dropdown is open
+       $anchor.addClass("hover");
+        
+        // make sure dropdown appears directly below parent list item    
+        $list
+            .show()
+            .css({
+                paddingTop: $container.data("origHeight")
+            });
+        
+        // don't do any animation if list shorter than max
+        if (multiplier > 1) {
+            $container
+                .css({
+                    height: maxHeight,
+                    overflow: "hidden"
+                })
+                .mousemove(function(e) {
+                    var offset = $container.offset();
+                    var relativeY = ((e.pageY - offset.top) * multiplier) - ($container.data("origHeight") * multiplier);
+                    if (relativeY > $container.data("origHeight")) {
+                        $list.css("top", -relativeY + $container.data("origHeight"));
+                    };
+                });
+        }
+        
+    }, function() {
+    
+        var $el = $(this);
+        
+        // put things back to normal
+        $el
+            .height($(this).data("origHeight"))
+            .find("ul")
+            .css({ top: 0 })
+            .hide()
+            .end()
+            .find("a")
+            .removeClass("hover");
+    
+    });
+    
+    
+});
---
+++ scripts/registration.js
@@ -0,0 +1,58 @@
+(function($){
+	// Birth year list population
+	for (i = new Date().getFullYear(); i > 1900; i--) {
+    	$('#birthYear').append($('<option />').val(i).html(i));
+    	//$('.dropdown .dropdown-menu').append($('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'+i+'</a></li>'));
+
+	}
+
+	// Mall list toggle script
+	var cm = $(".see-all-malls");
+	cm.on('click', function(e){
+		e.preventDefault();
+		$(".all-malls-list").toggle("slow", function(){
+			if(cm.text() === "SEE MORE MALLS"){
+				cm.text("SEE LESS");
+			}else{
+				cm.text("SEE MORE MALLS");
+			}
+		}); 
+	});
+
+	
+
+	$("#modal-cancel").click(function(){
+		$("#model-unsubscribe").fadeOut();
+		$(".modal-backdrop").fadeOut();
+	})
+	
+	//Mall select script
+	$(".all-malls-list li input[type='checkbox']").change(function(elem){
+		var self = $(this),
+			mval = self.val(),
+			id = self.attr("id");
+
+		if(this.checked === true){
+			var data = "<li class='pink-chkmark' data-id="+id+">" + mval + "</li>";
+			$(data).appendTo(".selected-mall ul").slideDown('slow');
+			$("#mall-select").html(" ");
+		} else{
+			$(".selected-mall").find('li[data-id="' + id + '"]').slideUp("slow", function(){
+				$(this).remove();
+			});
+		}
+	});
+	$("#registration_form input[type='checkbox']").click(function(){
+		$(this).closest("label").find("span").toggleClass("custom-checkmark-chkd");
+	});
+	
+	//Mall select validation
+	$(document).on('submit', '#registration_form', function () {
+		if ($("input[type='checkbox'][name='mall[]']:checked").size() > 0){
+				$("#mall-select").html(" ");
+			} else{
+				$("#mall-select").html("<ul class='list-unstyled'><li>Please select at least one mall.</li></ul>");
+				return false;
+			}
+	});
+})(jQuery); 
